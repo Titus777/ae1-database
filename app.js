6141a0af5ccc10cade6d47bc40afcb3a899ff4eb
Titus777 <4mahet92@solent.ac.uk>
2021-12-09 12:48:52 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
