3b3ffae2311925da5c6b6c44257cef5f1b8e3b6a
Titus777 <4mahet92@solent.ac.uk>
2021-12-09 12:54:01 +0000

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const bodyParser = require("body-parser");
 const expressSession = require("express-session");
+
+const { PORT, MONGODB_URI } = process.env;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+    console.error(err);
+    console.log(
+        "MongoDB connection error. Please make sure MongoDB is running.",
+        chalk.red("✗")
+    );
+    process.exit();
+});
