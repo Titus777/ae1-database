876b92fd5d32775f87d8435d18d5349783b1d4db
Titus777 <4mahet92@solent.ac.uk>
2021-12-13 16:06:46 +0000

    app.js and ejs

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path =require('path'); 
 
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.send("Crud Application");
+dotenv.config ({path:'config.env'})
+const PORT = process.env.PORT||8080
+
+// log requests
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view engine
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs"))
+
+//load design
+app.use('/css',express.static(path.resolve(__dirname,"design/css")))
+app.use('/background',express.static(path.resolve(__dirname,"design/background")))
+app.use('/javascript',express.static(path.resolve(__dirname,"design/javascript")))
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
 })
 
-app.listen(2020,()=>{console.log('Server is running on http://localhost:2020')});
+app.listen(PORT,()=>{console.log('Server is running on http://localhost:PORT')});
